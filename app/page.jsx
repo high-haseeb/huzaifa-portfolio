67747c907ca4f634b55a871979a3bef55a47cdf0
high-haseeb <haseebkhalidoriginal@gmail.com>
2023-11-14 03:28:01 +0500

    added react-spring

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Hero from "@/components/hero"
+
+function Page() {
+  return (
+    <div>
+      <Hero/>
+      <div className='bg-orange-500 w-screen h-screen'>
+        placeholder
+      </div>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Page
+
